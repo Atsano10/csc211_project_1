ff05e59230b9a3b242bd47bbf9591b77fcabd681
Atsano10 <161909284+Atsano10@users.noreply.github.com>
2025-11-06 14:58:54 -0500

    Refactor toUpper module for clarity and efficiency
    Refactor toUpper module to simplify logic and improve readability.

--- toUpper.v
+++ toUpper.v
@@ -1,15 +1,13 @@
 `timescale 1ns / 1ps
 
-module toUpper (a,b);
-    input  [7:0] a;
+module toUpper(a,b);
+    input [7:0] a;
     output [7:0] b;
 
     wire na7, na6, na4, na3, na2, na1, na0;
-    wire t1, t2, t3, t4;
-    wire sum;
-    wire simplified, notsimplified;
-
-    not #5 (na7, a[7]);
+    wire t1, t2, t3, t4, t5;
+    
+    not #5 (na7, a[7]); 
     not #5 (na6, a[6]);
     not #5 (na4, a[4]);
     not #5 (na3, a[3]);
@@ -17,16 +15,13 @@ module toUpper (a,b);
     not #5 (na1, a[1]);
     not #5 (na0, a[0]);
 
-    and #10 (t1, na7, a[6], a[5], na4, (a[3] | a[2] | a[1] | a[0]));
-    and #10 (t2, na7, a[6], a[5], a[4], na3);
-    and #10 (t3, na7, a[6], a[5], a[4], a[3], na2);
-    and #10 (t4, na7, a[6], a[5], a[4], a[3], a[2], na1, na0);
+    and #10 (t1, a[7], a[6]);
+    and #10 (t2, na7, na6, a[5]);
+    and #10 (t3, na7, a[6], a[5], a[4], a[3],a[2]);
+    and #10 (t4, na7, a[6], a[5],a[4],a[3],na2,a[1],a[0]);
+    and #10 (t5, na7, a[5], na4, na3, na2, na1, na0);
 
-    or #10 (sum, t1, t2, t3, t4);
-
-    and #10 (simplified, sum, a[5]);
-    not #5 (notsimplified, simplified);
-    and #10 (b[5], a[5], notsimplified);
+    or #10 (b[5],t1,t2,t3,t4,t5);
 
     buf #4 (b[0], a[0]);
     buf #4 (b[1], a[1]);
@@ -35,4 +30,5 @@ module toUpper (a,b);
     buf #4 (b[4], a[4]);
     buf #4 (b[6], a[6]);
     buf #4 (b[7], a[7]);
+
 endmodule
